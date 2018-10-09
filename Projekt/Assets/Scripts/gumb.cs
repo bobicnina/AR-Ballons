1c9373f1970f9616459cec79635bc07d2c3b189d
nina <ninab@artrebel9.com>
2018-10-09 17:21:15 +0200

    Initial commit

---
+++ Projekt/Assets/Scripts/gumb.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class gumb : MonoBehaviour {
+
+	System.Random r = new System.Random(DateTime.Now.Millisecond);
+ 
+ 	public Color RandomColor(){
+    				byte red = (byte)r.Next(0, 255);
+    				byte green = (byte)r.Next(0, 255);
+    				byte blue = (byte)r.Next(0, 255);
+ 
+			return new Color(red, green, blue);  }
+	
+	public Renderer Rend;
+
+
+	public void buttonPressed(){
+		GetComponent<Renderer>().material.color = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+		//Rend.sharedMaterial.color = RandomColor();
+	}
+}
+
